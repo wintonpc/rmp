4b205b5e6d313d4daeeebd6eec1537d07f5dedbd
Peter Winton <pwinton@indigobio.com>
2016-04-06 11:47:02 -0400

    wip

--- lib/server.rb
+++ lib/server.rb
@@ -1,28 +1,32 @@
 require 'socket'
 
 class Server
-  def start(port)
-    log "starting server"
-    ss = TCPServer.new(port)
-    log "listening on #{port}"
-    loop {
-      Thread.start(ss.accept) { |s|
-        log "accepted client"
-        begin
-          while line = s.gets;  # Returns nil on EOF.
-            (s << "You wrote: #{line.inspect}\r\n").flush
+  class << self
+    def start(port)
+      log "starting server"
+      ss = TCPServer.new(port)
+      log "listening on #{port}"
+      loop {
+        Thread.start(ss.accept) { |s|
+          log "accepted client"
+          begin
+            while line = s.gets;  # Returns nil on EOF.
+              (s << "You wrote: #{line.inspect}\r\n").flush
+            end
+          rescue
+            bt = $!.backtrace * "\n  "
+            ($stderr << "error: #{$!.inspect}\n  #{bt}\n").flush
+          ensure
+            s.close
           end
-        rescue
-          bt = $!.backtrace * "\n  "
-          ($stderr << "error: #{$!.inspect}\n  #{bt}\n").flush
-        ensure
-          s.close
-        end
+        }
       }
-    }
-  end
+    end
 
-  def log(s)
-    File.write('server.log', "#{s}\n")
+    def log(s)
+      File.open('/tmp/server.log', 'a') do |f|
+        f.puts s
+      end
+    end
   end
 end
