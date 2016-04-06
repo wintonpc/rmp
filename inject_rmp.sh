54443f6d0508d9c992f94fed8c4819a77210a0a7
Peter Winton <pwinton@indigobio.com>
2016-04-06 12:31:16 -0400

    wip

--- inject_rmp.sh
+++ inject_rmp.sh
@@ -5,6 +5,5 @@
 #RMP_PATH="$SCRIPT_DIR/lib/rmp.rb"
 #bundle exec rbtrace -p "$PID" -e "require_relative \"$RMP_PATH\"; Rmp.new.methods"
 
-git clone https://github.com/wintonpc/rmp
 echo 'require_relative "../rmp/lib/rmp"; Rmp.start_server(9786)' >> config/application.rb
 kill -HUP `cat tmp/unicorn.pid`
