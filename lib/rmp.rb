55540f0b5256f6bcf33f6e30830a499621adb34f
Peter Winton <pwinton@indigobio.com>
2016-04-06 11:37:53 -0400

    wip

--- lib/rmp.rb
+++ lib/rmp.rb
@@ -2,14 +2,16 @@ require 'objspace'
 require_relative './server'
 
 class Rmp
-  def dump_all(file_path)
-    GC.start
-    File.open(file_path, 'w') do |f|
-      ObjectSpace.dump_all(output: f)
+  class << self
+    def dump_all(file_path)
+      GC.start
+      File.open(file_path, 'w') do |f|
+        ObjectSpace.dump_all(output: f)
+      end
     end
-  end
 
-  def start_server(port)
-    Server.new.start(port)
+    def start_server(port)
+      Server.new.start(port)
+    end
   end
 end
