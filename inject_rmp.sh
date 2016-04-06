94d687814a2f5109113c69d1040cfc2de824e314
Peter Winton <pwinton@indigobio.com>
2016-04-06 11:41:56 -0400

    wip

--- inject_rmp.sh
+++ inject_rmp.sh
@@ -3,4 +3,4 @@
 PID=$1
 SCRIPT_DIR="$(dirname $(readlink -f $0))"
 RMP_PATH="$SCRIPT_DIR/lib/rmp.rb"
-bundle exec rbtrace -p "$PID" -e "Thread.new{puts Object.inspect; require_relative \"$RMP_PATH\"; Rmp.start_server(9786)}"
+bundle exec rbtrace -p "$PID" -e "puts Object.inspect; Thread.new{puts Object.inspect; require_relative \"$RMP_PATH\"; Rmp.start_server(9786)}"
